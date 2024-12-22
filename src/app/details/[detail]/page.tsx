5263ddb76940a3cab27deacf7aa7b4b7c0ef55de
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-12-22 18:54:26 +0500

    dd

--- src/app/details/[detail]/page.tsx
+++ src/app/details/[detail]/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 interface BlogDetailProps {
   params: {
     detail: string; // The dynamic route parameter
-    [key: string]: any;
+    [key: string]: unknown;
   };
 }
 
