6186473e63988bc8ebc7d8b0dafb62fe2560606e
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-12-22 21:22:26 +0500

    f

--- src/app/details/[detail]/page.tsx
+++ src/app/details/[detail]/page.tsx
@@ -2,29 +2,13 @@ import React from 'react';
 import blogData from '@/blogs/blogs';
 import styles from "../../../styles/BlogDetail.module.css";
 import CommentSection from '@/components/CommentSection';
-import Image from 'next/image';
 
-interface BlogDetailProps {
-  params: {
-    detail: string; // The dynamic route parameter
-    [key: string]: unknown;
-  };
-}
-
-interface Blog {
-  id: string;
-  title: string;
-  excerpt: string;
-  author: string;
-  date: string;
-  image: string;
-  content: string;
-}
-
-// The component should be a regular function, not async
-const BlogDetail: React.FC<BlogDetailProps> = ({ params }) => {
-  const { detail } = params; // Extract the detail parameter
-  const blog: Blog | undefined = blogData.find((b) => b.id === detail);
+const BlogDetail = async (props:any) => {
+  
+  console.log("Prints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar toPrints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar toPrints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar toPrints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar toPrints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar toPrints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar to")
+  const amm = await props.params;
+  const id = amm.detail;
+  const blog = blogData.find((b) => b.id === id);
 
   if (!blog) {
     return <p style={{ textAlign: "center", padding: "2rem" }}>Blog not found.</p>;
@@ -32,23 +16,23 @@ const BlogDetail: React.FC<BlogDetailProps> = ({ params }) => {
 
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1>{blog.title}</h1>
-          <p className={styles.excerpt}>{blog.excerpt}</p>
-          <div className={styles.meta}>
-            <span>By {blog.author}</span>
-            <span>{blog.date}</span>
-          </div>
-        </div>
-        <div className={styles.imageWrapper}>
-          <Image src={blog.image} alt={blog.title} width={300} height={300} className={styles.image} />
-        </div>
-        <div className={styles.content}>
-          <p>{blog.content}</p>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1> {blog.title}</h1>
+        <p className={styles.excerpt}>{blog.excerpt}</p>
+        <div className={styles.meta}>
+          <span>By {blog.author}</span>
+          <span>{blog.date}</span>
         </div>
       </div>
-      <CommentSection />
+      <div className={styles.imageWrapper}>
+        <img src={blog.image} alt={blog.title} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <p>{blog.content}</p>
+      </div>
+    </div>
+    <CommentSection />
     </>
   );
 };
